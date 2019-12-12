710eeec44c98a16dab3c71cd956ab16194f181a0
Joseph D. Puerari <joep@Joes-MBP.fios-router.home>
2019-12-11 20:58:08 -0500

    password

---
+++ script.js
@@ -0,0 +1,91 @@
+//step 1: prompt the user
+prompt("Welcome! This is a password generator. Before continuing further, please set up a password")
+
+//step 2
+
+  //step a
+
+  //step b
+
+  //step c
+
+//step 3
+
+  //step a
+
+  //step b
+
+  //step c
+
+//step 4
+
+  //step a
+
+  //step b
+
+  //step c
+
+//step 5
+
+  //step a
+
+  //step b
+
+  //step c
+
+//step 6
+
+  //step a
+
+  //step b
+
+  //step c
+
+//step 7
+
+  //step a
+
+  //step b
+
+  //step c
+
+//step 8
+
+  //step a
+
+  //step b
+
+  //step c
+
+//step 9
+
+  //step a
+
+  //step b
+
+  //step c
+
+//step 10
+
+  //step a
+
+  //step b
+
+  //step c
+
+//step 11
+
+  //step a
+
+  //step b
+
+  //step c
+
+//step 12
+
+  //step a
+
+  //step b
+
+  //step c
+
