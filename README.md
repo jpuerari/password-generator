1d6155a7e4e9f875dfbbb93c3a7cc059bac29eb3
Joseph D. Puerari <joep@Joes-MacBook-Pro.local>
2019-12-14 09:48:41 -0500

    updated

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 Create a password generator via JavaScript.
 
 # Resources Consulted:
-W3schools, MDN, YouTube, office hours and tutoring.
+W3schools, MDN, YouTube, class videos and materials, office hours and tutoring.
 
 # Purpose of Project:
 Create an app that takes in user input and outputs the necessary characters as requested.
@@ -15,7 +15,12 @@ HTML, CSS, and JavaScript (DOM included).
 # Outcome:
 Applying logic and different resources to construct a functioning web app.
 
+# Areas that Need Work
+Fixing the "double loop" in the app.
+Allowing the user to cancel out of the window alerts when needed
+Copying the password to clipboard
 
-
+# Conclusion 
+Extremely challenging, yet rewarding to see your code functioning.
 
 
--- script.js
+++ script.js
@@ -15,67 +15,73 @@ alert("welcome, please click the generate button below");
 
 
 
-function makeAPassword(){
-var length = prompt("Before continuing further, please choose a length that is 8-128 characters long.");
-console.log(length)
-if (isNaN(length)){
-  alert("Please enter an appropriate length");
-}
+function makeAPassword() {
+  var length = prompt("Before continuing further, please choose a length that is 8-128 characters long.");
+  console.log(length)
+  if (isNaN(length)) {
+    alert("Please enter an appropriate length");
+    return;
+  }
 
-var symbolChoice = confirm("Do you want special characters?");
-console.log(symbolChoice)
-if(symbolChoice === true){
-  possibleCharacters = possibleCharacters + symbolCharacters;
-}
-
-
-var upperChoice = confirm("Do you want upper case characters?");
-if(upperChoice === true){
-  possibleCharacters = possibleCharacters + upperCaseCharacters;
-}
-
-// confirm will result in true or false, you can use it right away instead of storing it in a variable that you'll immediately use
-if(confirm("Do you want lower case characters?") === true){
-  possibleCharacters = possibleCharacters + lowerCaseCharacters;
-}
-
-var numberChoice = confirm("Do you want number characters?");
-if(numberChoice === true){
-  possibleCharacters = possibleCharacters + numberCharacters;
-}
-
-console.log(possibleCharacters)
+  var symbolChoice = confirm("Do you want special characters?");
+  console.log(symbolChoice)
+  if (symbolChoice === true) {
+    possibleCharacters = possibleCharacters + symbolCharacters;
+  }
 
 
-//3rd step: writing out the functions
-function password (i,characters) {
-  var pwd = " ";
- }
+  var upperChoice = confirm("Do you want upper case characters?");
+  if (upperChoice === true) {
+    possibleCharacters = possibleCharacters + upperCaseCharacters;
+  }
 
-//4th step: write a for loop so user cancel out
-for(var i = 0; i<length; i++){
-  var num = Math.floor(Math.random() * possibleCharacters.length)
-  var randomChar = possibleCharacters[num];
-  //upperCaseCharacters.charAt(num);
-  finalPassword = finalPassword + randomChar;
-}
+  // confirm the result in true or false booleans, you can use it right away instead of storing it in a variable that you'll immediately use
+  if (confirm("Do you want lower case characters?") === true) {
+    possibleCharacters = possibleCharacters + lowerCaseCharacters;
+  }
 
-console.log(finalPassword)
-document.getElementById("password").value = finalPassword;
+  var numberChoice = confirm("Do you want number characters?");
+  if (numberChoice === true) {
+    possibleCharacters = possibleCharacters + numberCharacters;
+  }
+
+  console.log(possibleCharacters)
+
+
+  //3rd step: writing out the functions
+  function password(i, characters) {
+    var pwd = " ";
+  }
+
+  // if there's nothing in possibleCharacters, stop function
+  if (!possibleCharacters) {
+    alert("You need to tell us what characters you want!");
+    return;
+  }
+
+  //4th step: write a for loop so user cancel out
+  for (var i = 0; i < length; i++) {
+    var num = Math.floor(Math.random() * possibleCharacters.length)
+    var randomChar = possibleCharacters[num];
+    //upperCaseCharacters.charAt(num);
+    finalPassword = finalPassword + randomChar;
+  }
+
+  console.log(finalPassword)
+  document.getElementById("password").value = finalPassword;
 }
 
 var button = document.getElementById("generate");
 button.addEventListener("click", makeAPassword);
 
 //function to copy password to clipboard
-function copyPassword(){
+
   document.getElementById("copyToClipBoard").select();
   document.execCommand("Copy");
   alert("Password copied to clipboard!")
 }
 
- //add password to textbox/display area
- document.getElementById("display").value = password;
+//add password to textbox/display area
 
 
 
